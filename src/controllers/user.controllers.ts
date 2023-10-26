0d28996e53019a663602f4a882cbe873e6610423
Rafiul29 <rafiulislam4472@gmail.com>
2023-10-27 00:17:40 +0600

    user controllers added

---
+++ src/controllers/user.controllers.ts
@@ -0,0 +1,101 @@
+import { Request, Response } from "express";
+import mongoose from "mongoose";
+import UserModel from "../models/user.model";
+import { handleError } from "../errors/handle.error";
+
+export class UserController {
+  constructor() { }
+  
+  public async getAnUser(req: Request, res: Response): Promise<void> {
+    try {
+
+      const { uid } = req.params;
+      const userId = req.user?._id;
+
+      if (!mongoose.Types.ObjectId.isValid(uid)) {
+        res.status(404).json({ message: "user not found" });
+        return
+      }
+
+      if (uid !== userId.toString()) {
+        res.status(403).json({ message: "Forbidden" });
+        return
+      }
+
+      await Promise.resolve().then(async() => {
+        const user = await UserModel.findById(uid);
+        res.status(200).json(user)
+      })
+
+    } catch (error:unknown) {
+      await handleError(error,res);
+    }
+
+  }
+
+  public async deleteAnUser(req: Request, res: Response): Promise<void> {
+try {
+
+      const { uid } = req.params;
+      const userId = req.user?._id;
+
+      if (!mongoose.Types.ObjectId.isValid(uid)) {
+        res.status(404).json({ message: "user not found" });
+        return
+      }
+
+      if (uid !== userId.toString()) {
+        res.status(403).json({ message: "Forbidden" });
+        return
+      }
+
+      await Promise.resolve().then(async() => {
+        const user = await UserModel.findByIdAndDelete(uid);
+        res.status(200).json(user)
+      })
+
+    } catch (error:unknown) {
+      await handleError(error,res);
+    }
+  }
+
+  public async updateAnUser(req: Request, res: Response): Promise<void> {
+    try {
+
+      const { uid } = req.params;
+      const userId = req.user?._id;
+      const {name,photoUrl,address,phoneNumber}=req.body;
+
+      if (!mongoose.Types.ObjectId.isValid(uid)) {
+        res.status(404).json({ message: "user not found" });
+        return
+      }
+
+      if (uid !== userId.toString()) {
+        res.status(403).json({ message: "Forbidden" });
+        return
+      }
+
+      await Promise.resolve().then(async() => {
+        const user = await UserModel.findByIdAndUpdate(uid,{name,photoUrl,address,phoneNumber},{new:true});
+        res.status(200).json(user)
+      })
+
+    } catch (error:unknown) {
+      await handleError(error,res);
+    }
+  }
+
+
+  public async getAllAnUsers(req: Request, res: Response): Promise<void> {
+    try {    
+      await Promise.resolve().then(async() => {
+        const user = await UserModel.find({});
+        res.status(200).json(user)
+      })
+    } catch (error:unknown) {
+      await handleError(error,res);
+    }
+  }
+
+}
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,18 +1,22 @@
 import express,{Router} from "express";
+import AuthMiddleware from "../middlewares/auth.middlewares";
+import { UserController } from "../controllers/user.controllers";
 
 const userRouter:Router=express.Router();
 
-// get all users
-userRouter.get("/")
+const authInstance=new AuthMiddleware();
+const userInstance=new UserController();
 
 //get an user
-userRouter.get("/:uid");
+userRouter.get("/:uid",authInstance.isAuthenticated,userInstance.getAnUser);
 
 // delete an user
-userRouter.delete("/:uid");
+userRouter.delete("/:uid",authInstance.isAuthenticated,userInstance.deleteAnUser);
 
 // update and user
-userRouter.put("/:id")
+userRouter.put("/:uid",authInstance.isAuthenticated,userInstance.updateAnUser)
 
+// get all users
+userRouter.get("/",authInstance.isAuthenticated,authInstance.isAdmin,userInstance.getAllAnUsers)
 
 export default userRouter;
